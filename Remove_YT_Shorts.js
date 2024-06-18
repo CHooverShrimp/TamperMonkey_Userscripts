2e2c22405b969ecdd12979c5d7a6a589717ddb6a
CHooverShrimp <choovershrimp@gmail.com>
2024-06-18 07:44:44 -0700

    Update Remove_YT_Shorts.js
    rewrote the whole thing to be faster

--- Remove_YT_Shorts.js
+++ Remove_YT_Shorts.js
@@ -1,39 +1,27 @@
 // ==UserScript==
 // @name         YouTube Short Remover
 // @namespace    https://github.com/CHooverShrimp
-// @version      0.1
+// @version      1.0
 // @description  Removing brainrot automatically just for (You)!
 // @author       Hoover
-// @match        *://www.youtube.com/*
+// @match        *://*.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // @license      MIT
 // ==/UserScript==
 
 (function () {
-    'use strict';
-
-    // Function to remove elements by class name
-    function removeElementsByClassName(className) {
-        var elements = document.getElementsByClassName(className);
-        while (elements.length > 0) {
-            elements[0].parentNode.removeChild(elements[0]);
-        }
+    let css = `
+     #dismissible.ytd-rich-shelf-renderer{
+      display:none;
+     }
+    `;
+    if (typeof GM_addStyle !== "undefined") {
+        GM_addStyle(css);
+    } else {
+        let styleNode = document.createElement("style");
+        styleNode.appendChild(document.createTextNode(css));
+        (document.querySelector("head") || document.documentElement).appendChild(styleNode);
     }
-
-    // Initial removal on page load
-    removeElementsByClassName('ytd-rich-section-renderer');
-
-    // Mutation observer to catch dynamically loaded content
-    var observer = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-            if (mutation.addedNodes.length) {
-                removeElementsByClassName('ytd-rich-section-renderer');
-            }
-        });
-    });
-
-    // Observe changes in the body
-    observer.observe(document.body, { childList: true, subtree: true });
 })();
 
